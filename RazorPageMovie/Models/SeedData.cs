d7ddcdd8fa94fc374bd8cf37a74321a550f7be48
Marikrmen <carmen-senti@hotmail.com>
2023-11-10 23:48:00 -0600

    first commit

---
+++ RazorPageMovie/Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new RazorPageMovieContext1(
+        serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPageMovieContext1>>()))
+        {
+            if (context == null || context.Movie == null)
+            {
+                throw new ArgumentNullException("Null RazorPagesMovieContext");
+            }
+
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    RealeseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "R"
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    RealeseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M,
+                    Rating = "G"
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    RealeseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M,
+                    Rating="G"
+                },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    RealeseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating="NA"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
